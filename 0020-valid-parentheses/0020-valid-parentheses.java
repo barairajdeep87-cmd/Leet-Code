c98368f792379e24fdb5d1c79768fe66dc1fa3a5
barairajdeep87-cmd <barairajdeep87@gmail.com>
2026-08-13 15:48:39 +0530

    Time: 3 ms (86.08%), Space: 43.2 MB (73.38%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character>stack=new Stack<>();
+        char[] arr=s.toCharArray();
+       
+        for(char c:arr){
+            if(c=='('){
+                stack.push(')');
+            }else if(c=='{'){
+                stack.push('}');
+            }else if(c=='['){
+                stack.push(']');
+            }else if(stack.isEmpty() || stack.pop()!=c){
+                return false;
+            }
+        }if(!stack.isEmpty()) return false;
+        return true;
+    }
+}
