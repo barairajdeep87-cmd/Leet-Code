e0133ce85a49ca096ed7af9426d286be47b6666b
barairajdeep87-cmd <barairajdeep87@gmail.com>
2026-08-30 18:27:48 +0530

    Time: 2 ms (91.93%), Space: 47.8 MB (57.58%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int i,j=0;
+        int temp=0;
+        for(i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+                temp=nums[j];
+                nums[j]=nums[i];
+                nums[i]=temp;
+                j++;
+            }
+        }
+    }
+}
